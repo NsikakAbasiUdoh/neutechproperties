f40b19b143e56baaa0b6666708f28fba6f7bb361
Nsikakeu <nsikakabasieudoh@gmail.com>
2026-02-13 16:12:53 +0100

    New code

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import React, {
   lazy,
   ErrorInfo,
   ReactNode,
+  Component,
 } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -42,16 +43,13 @@ interface ErrorBoundaryState {
 }
 
 // Error Boundary Component
-class ErrorBoundary extends React.Component<
-  ErrorBoundaryProps,
-  ErrorBoundaryState
-> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props);
-    this.state = { hasError: false, error: null };
-  }
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  public state: ErrorBoundaryState = {
+    hasError: false,
+    error: null,
+  };
 
-  static getDerivedStateFromError(error: Error) {
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
     return { hasError: true, error };
   }
 
